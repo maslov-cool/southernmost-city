3156fc4d5074fe8eee477a6cc6a761a507428a96
alex <106116143+maslov-cool@users.noreply.github.com>
2025-02-17 23:36:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+
+
+def main(geocode):
+    server_address = 'http://geocode-maps.yandex.ru/1.x/?'
+    api_key = '8013b162-6b42-4997-9691-77b7074026e0'
+    # Готовим запрос.
+    geocoder_request = f'{server_address}apikey={api_key}&geocode={geocode}&format=json'
+
+    # Выполняем запрос.
+    response = requests.get(geocoder_request)
+    if response:
+        # Преобразуем ответ в json-объект
+        json_response = response.json()
+
+        # Получаем первый топоним из ответа геокодера.
+        # Согласно описанию ответа, он находится по следующему пути:
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        # Полный адрес топонима:
+        toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+        # Координаты центра топонима:
+        toponym_coodrinates = toponym["Point"]["pos"]
+        # Печатаем извлечённые из ответа поля:
+        return float(toponym_coodrinates.split()[1])
+    else:
+        print("Ошибка выполнения запроса:")
+        print(geocoder_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+
+
+A = []
+for i in input().split(', '):
+    A.append([i, main(i)])
+print('Самый южный город: ' + min(A, key=lambda x: x[1])[0])
